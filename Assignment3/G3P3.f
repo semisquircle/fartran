6ec772b9c685fceb44a636f06a43e10696f71f58
semisquircle <semisquircle@gmail.com>
2025-09-30 08:17:53 -0400

    assignment 3

---
+++ Assignment3/G3P3.f
@@ -0,0 +1,8 @@
+      PROGRAM G3P3
+c ----------------------------------------------
+c Assignment 3: The Matrix Add Program
+c Group 3 (Shawn Gallagher, Lucas Giovannelli)
+c ----------------------------------------------
+      IMPLICIT NONE
+
+      END
