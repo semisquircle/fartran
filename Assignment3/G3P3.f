3841480468daf43e7814e046c3d28dab6b754591
semisquircle <semisquircle@gmail.com>
2025-09-30 08:26:48 -0400

    hot garbage

--- Assignment3/G3P3.f
+++ Assignment3/G3P3.f
@@ -5,4 +5,20 @@ c Group 3 (Shawn Gallagher, Lucas Giovannelli)
 c ----------------------------------------------
       IMPLICIT NONE
 
+      LOGICAL file_exists
+	  CHARACTER(len=20) file_name
+      INTEGER flag
+
+      file_exists = .FALSE.
+	  flag = 0
+
+      PRINT *, 'Please enter input file name: '
+      DO WHILE (.NOT. file_exists)
+  100  FORMAT(A)
+	   READ(*, 100), file_name
+       INQUIRE(FILE=TRIM(file_name), EXIST=file_exists)
+	  END DO
+	  
+	  PRINT *, file_exists
+
       END
