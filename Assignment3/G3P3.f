ab3291484146bdcefd053317856c5404bbe418a5
AhabNStarbuck <GIO07221@pennwest.edu>
2025-09-30 08:49:17 -0400

    balls

--- Assignment3/G3P3.f
+++ Assignment3/G3P3.f
@@ -6,7 +6,7 @@ c ----------------------------------------------
       IMPLICIT NONE
 
       LOGICAL file_exists
-	  CHARACTER(len=20) file_name
+	  CHARACTER(len=20) file_name_in, file_name_out
       INTEGER flag
 
       file_exists = .FALSE.
@@ -14,11 +14,28 @@ c ----------------------------------------------
 
       PRINT *, 'Please enter input file name: '
       DO WHILE (.NOT. file_exists)
-  100  FORMAT(A)
-	   READ(*, 100), file_name
-       INQUIRE(FILE=TRIM(file_name), EXIST=file_exists)
+	   READ (*, *) file_name_in
+       INQUIRE(FILE=TRIM(file_name_in), EXIST=file_exists)
 	  END DO
 	  
+      OPEN (UNIT=10, FILE=TRIM(file_name_in), STATUS='old', IOSTAT=flag)
+      IF (flag /= 0) THEN
+         PRINT *, 'Error opening input file.'
+         STOP
+      END IF
+
+      PRINT *, 'Please enter input file name: '
+      DO WHILE (.NOT. file_exists)
+	   READ (*, *) file_name_out
+       INQUIRE(FILE=TRIM(file_name_out), EXIST=file_exists)
+	  END DO
+	  
+      OPEN (UNIT=10, FILE=TRIM(file_name_out), STATUS='old', IOSTAT=flag)
+      IF (flag /= 0) THEN
+         PRINT *, 'Error opening output file.'
+         STOP
+      END IF
+
 	  PRINT *, file_exists
 
       END
