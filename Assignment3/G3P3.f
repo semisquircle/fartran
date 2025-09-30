616342d3d23f4de34d10211f62a7efa298cb21bc
semisquircle <semisquircle@gmail.com>
2025-09-30 09:00:42 -0400

    1% done

--- Assignment3/G3P3.f
+++ Assignment3/G3P3.f
@@ -5,37 +5,40 @@ c Group 3 (Shawn Gallagher, Lucas Giovannelli)
 c ----------------------------------------------
       IMPLICIT NONE
 
-      LOGICAL file_exists
-	  CHARACTER(len=20) file_name_in, file_name_out
+      LOGICAL file_in_exists, file_out_exists
+      CHARACTER(len=20) file_name_in, file_name_out
       INTEGER flag
 
-      file_exists = .FALSE.
-	  flag = 0
+      file_in_exists = .FALSE.
+      file_out_exists = .FALSE.
+      flag = 0
 
-      PRINT *, 'Please enter input file name: '
-      DO WHILE (.NOT. file_exists)
-	   READ (*, *) file_name_in
-       INQUIRE(FILE=TRIM(file_name_in), EXIST=file_exists)
-	  END DO
-	  
-      OPEN (UNIT=10, FILE=TRIM(file_name_in), STATUS='old', IOSTAT=flag)
-      IF (flag /= 0) THEN
-         PRINT *, 'Error opening input file.'
-         STOP
+      PRINT *, 'Please enter an input file name: '
+      DO WHILE (.NOT. file_in_exists)
+       READ(*, *) file_name_in
+       INQUIRE(FILE=TRIM(file_name_in), EXIST=file_in_exists)
+       IF (.NOT. file_in_exists) THEN
+        PRINT *, 'File does not exist, please try again: '
+       END IF
+      END DO
+      
+      OPEN(UNIT=10, FILE=TRIM(file_name_in), STATUS='old', IOSTAT=flag)
+      IF (flag .NE. 0) THEN
+       PRINT *, 'Error opening input file.'
+       STOP
       END IF
 
-      PRINT *, 'Please enter input file name: '
-      DO WHILE (.NOT. file_exists)
-	   READ (*, *) file_name_out
-       INQUIRE(FILE=TRIM(file_name_out), EXIST=file_exists)
-	  END DO
-	  
-      OPEN (UNIT=10, FILE=TRIM(file_name_out), STATUS='old', IOSTAT=flag)
-      IF (flag /= 0) THEN
-         PRINT *, 'Error opening output file.'
-         STOP
+      PRINT *, 'Please enter an output file name: '
+      READ(*, *) file_name_out
+      IF (.NOT. file_out_exists) THEN
+       PRINT *, 'Output file not found, creating new file...'
+       OPEN(UNIT=10, FILE=file_name_out, STATUS='NEW', ACTION='WRITE')
       END IF
-
-	  PRINT *, file_exists
+      
+c OPEN(UNIT=10, FILE=TRIM(file_name_out), STATUS='old', IOSTAT=flag)
+c IF (flag .NE. 0) THEN
+c  PRINT *, 'Error opening output file.'
+c  STOP
+c END IF
 
       END
