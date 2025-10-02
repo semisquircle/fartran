5363b4025d4f5c8956c7e2104d90752d37f4b905
semisquircle <semisquircle@gmail.com>
2025-10-01 23:33:17 -0400

    good progress

--- Assignment3/G3P3.f
+++ Assignment3/G3P3.f
@@ -4,47 +4,125 @@ c Assignment 3: The Matrix Add Program
 c Group 3 (Shawn Gallagher, Lucas Giovannelli)
 c ----------------------------------------------
       IMPLICIT NONE
-c Variable Declarations
-      LOGICAL file_in_exists, file_out_exists
-      CHARACTER(len=20) file_name_in, file_name_out
-      INTEGER flag
 
+c     Declare variables
+      INTEGER MAXROWS, MAXCOLS
+      PARAMETER (MAXROWS=10, MAXCOLS=10)
+
+      REAL matrix(MAXROWS, MAXCOLS)
+      REAL matrix_sum(MAXROWS, MAXCOLS)
+
+      INTEGER flag, num_matrices, rows, cols, i, j, mat_count
+      LOGICAL file_in_exists, file_out_exists
+      CHARACTER*20 file_name_in, file_name_out, response
+
+c     Initialize
       file_in_exists = .FALSE.
       file_out_exists = .FALSE.
       flag = 0
-c User Input for File Names
-      PRINT *, 'Please enter an input file name: '
-      DO WHILE (.NOT. file_in_exists)
-       READ(*, *) file_name_in
-       INQUIRE(FILE=TRIM(file_name_in), EXIST=file_in_exists)
-      END IF
-       IF (.NOT. file_in_exists) THEN
-       PRINT *, 'File does not exist, please try again: '
-       ELSEIF (file_name_in .EQ. 'QUIT') THEN
-       PRINT *, 'Exiting program...'
-       STOP
-       END IF
+
+c     Initialize sum matrix to zero
+      DO i = 1, MAXROWS
+       DO j = 1, MAXCOLS
+        matrix_sum(i,j) = 0.0
+       END DO
       END DO
 
+c     Ask user for input file
+ 10   PRINT *, 'Please enter an input file name (or QUIT to exit):'
+      READ(*, *) file_name_in
+      IF (file_name_in .EQ. 'QUIT') STOP
+      INQUIRE(FILE=file_name_in, EXIST=file_in_exists)
+      IF (.NOT. file_in_exists) THEN
+       PRINT *, 'File does not exist, please try again.'
+       GO TO 10
+      END IF
 
-c Open input file    
-      OPEN(UNIT=10, FILE=TRIM(file_name_in), STATUS='old', IOSTAT=flag)
+c     Open input file
+      OPEN(UNIT=10, FILE=file_name_in, STATUS='OLD', IOSTAT=flag)
       IF (flag .NE. 0) THEN
        PRINT *, 'Error opening input file.'
        STOP
       END IF
-c Get output file name
-      PRINT *, 'Please enter an output file name: '
+
+c     Ask user for output file
+ 20   PRINT *, 'Please enter an output file name (or QUIT to exit):'
       READ(*, *) file_name_out
-      IF (.NOT. file_out_exists) THEN
-       PRINT *, 'Output file not found, creating new file...'
-       OPEN(UNIT=10, FILE=file_name_out, STATUS='NEW', ACTION='WRITE')
+      IF (file_name_out(1:4) .EQ. 'QUIT') STOP
+      INQUIRE(FILE=file_name_out, EXIST=file_out_exists)
+      IF (file_out_exists) THEN
+       PRINT *, 'File exists. Enter NEW name, OVERWRITE, or QUIT:'
+       READ(*, *) response
+       IF (response .EQ. 'QUIT') STOP
+       IF (response .EQ. 'OVERWRITE') THEN
+        OPEN(UNIT=20, FILE=file_name_out, STATUS='OLD', IOSTAT=flag)
+       ELSE
+        file_name_out = response
+        GO TO 20
+       END IF
+      ELSE
+       OPEN(UNIT=20, FILE=file_name_out, STATUS='NEW', IOSTAT=flag)
       END IF
-      
-c OPEN(UNIT=10, FILE=TRIM(file_name_out), STATUS='old', IOSTAT=flag)
-c IF (flag .NE. 0) THEN
-c  PRINT *, 'Error opening output file.'
-c  STOP
-c END IF
+
+      IF (flag .NE. 0) THEN
+       PRINT *, 'Error opening output file.'
+       STOP
+      END IF
+
+c     Read number of matrices
+      READ(10, *, IOSTAT=flag) num_matrices
+      IF (flag .NE. 0) THEN
+       PRINT *, 'Error reading number of matrices.'
+       STOP
+      END IF
+      IF (num_matrices .GT. 10) THEN
+       PRINT *, 'Number of matrices exceeds maximum of 10.'
+       STOP
+      END IF
+
+c     Read matrix dimensions
+      READ(10, *, IOSTAT=flag) rows, cols
+      IF (flag .NE. 0) THEN
+       PRINT *, 'Error reading matrix dimensions.'
+       STOP
+      END IF
+      IF (rows .GT. MAXROWS .OR. cols .GT. MAXCOLS) THEN
+       PRINT *, 'Matrix dimensions exceed maximum allowed size.'
+       STOP
+      END IF
+
+c     Loop over all matrices
+      DO mat_count = 1, num_matrices
+c      Read matrix
+       DO i = 1, rows
+        READ(10, *, IOSTAT=flag) (matrix(i,j), j=1,cols)
+        IF (flag .NE. 0) THEN
+         PRINT *, 'Error reading matrix from file.'
+         STOP
+        END IF
+       END DO
+
+c      Add to sum matrix
+       DO i = 1, rows
+        DO j = 1, cols
+         matrix_sum(i,j) = matrix_sum(i,j) + matrix(i,j)
+        END DO
+       END DO
+
+c      Print current matrix to output file
+       WRITE(20, *) 'MATRIX ', mat_count
+       DO i = 1, rows
+        WRITE(20,'(10F6.1)') (matrix(i,j), j=1,cols)
+       END DO
+      END DO
+
+c     Print sum matrix
+      WRITE(20, *) 'SUM OF ALL ', num_matrices, ' MATRICES'
+      DO i = 1, rows
+       WRITE(20,'(10F6.1)') (matrix_sum(i,j), j=1,cols)
+      END DO
+
+      CLOSE(10)
+      CLOSE(20)
 
       END
