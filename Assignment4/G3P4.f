f962e310561b215add640fde14318a4c99a76988
semisquircle <semisquircle@gmail.com>
2025-10-14 01:02:06 -0400

    math fixes + comments

--- Assignment4/G3P4.f
+++ Assignment4/G3P4.f
@@ -1,74 +1,76 @@
-      PROGRAM MATRIX_OPERATIONS
+      PROGRAM G3P4
 c ----------------------------------------------
 c Assignment 4: The Matrix Calculator Program
 c Group 3 (Shawn Gallagher, Lucas Giovannelli)
 c ----------------------------------------------
       IMPLICIT NONE
 
+c Variables
+c Note: 'm' refers to columns and 'n' refers to rows
+c (If we kept 'r' and 'c' we couldn't name the third matrix 'C')
       INTEGER, PARAMETER :: max = 10
       REAL, DIMENSION(max, max) :: A, B, C
-      INTEGER m1, n1, m2, n2, iopt
-      LOGICAL :: done = .FALSE.
+      INTEGER m1, n1, m2, n2, choice
+      LOGICAL :: quit = .FALSE.
 
-      DO WHILE (.NOT. done)
-       CALL show_menu(iopt)
-
-       IF (iopt .EQ. 1) THEN
-        CALL read_matrix(A, m1, n1, 'A')
-        CALL read_matrix(B, m2, n2, 'B')
-        IF (m1 .EQ. m2 .AND. n1 .EQ. n2) THEN
-         CALL add_matrix(A, B, C, m1, n1)
+c Main program loop, repeats until user quits
+      DO WHILE (.NOT. quit)
+       CALL menu(choice)
+       SELECT CASE (choice)
+      CASE (1)
+       CALL read_matrix(A, m1, n1, 'A')
+       CALL read_matrix(B, m2, n2, 'B')
+         IF (m1 .EQ. m2 .AND. n1 .EQ. n2) THEN
+          CALL add_matrices(A, B, C, m1, n1)
+          CALL print_matrix(A, m1, n1, 'A')
+          CALL print_matrix(B, m2, n2, 'B')
+          CALL print_matrix(C, m1, n1, 'A + B')
+         ELSE
+          PRINT *, 'Error: Matrices must have the same dimensions.'
+         ENDIF
+        CASE (2)
+         CALL read_matrix(A, m1, n1, 'A')
+         CALL read_matrix(B, m2, n2, 'B')
+         IF (m1 .EQ. m2 .AND. n1 .EQ. n2) THEN
+          CALL subtract_matrices(A, B, C, m1, n1)
+          CALL print_matrix(A, m1, n1, 'A')
+          CALL print_matrix(B, m2, n2, 'B')
+          CALL print_matrix(C, m1, n1, 'A - B')
+         ELSE
+          PRINT *, 'Error: Matrices must have the same dimensions.'
+         ENDIF
+        CASE (3)
+         CALL read_matrix(A, m1, n1, 'A')
+         CALL read_matrix(B, m2, n2, 'B')
+         IF (n1 .EQ. m2) THEN
+          CALL multiply_matrices(A, B, C, m1, n1, n2)
+          CALL print_matrix(A, m1, n1, 'A')
+          CALL print_matrix(B, m2, n2, 'B')
+          CALL print_matrix(C, m1, n2, 'A * B')
+         ELSE
+          PRINT *, 'Error: Inner dimensions must match.'
+         ENDIF
+        CASE (4)
+         CALL read_matrix(A, m1, n1, 'A')
+         CALL transpose_matrix(A, C, m1, n1)
          CALL print_matrix(A, m1, n1, 'A')
-         CALL print_matrix(B, m2, n2, 'B')
-         CALL print_matrix(C, m1, n1, 'A + B')
-        ELSE
-         PRINT *, 'Error: Matrices must have the same dimensions.'
-        ENDIF
-
-       ELSE IF (iopt .EQ. 2) THEN
-        CALL read_matrix(A, m1, n1, 'A')
-        CALL read_matrix(B, m2, n2, 'B')
-        IF (m1 .EQ. m2 .AND. n1 .EQ. n2) THEN
-         CALL subtract_matrix(A, B, C, m1, n1)
-         CALL print_matrix(A, m1, n1, 'A')
-         CALL print_matrix(B, m2, n2, 'B')
-         CALL print_matrix(C, m1, n1, 'A - B')
-        ELSE
-         PRINT *, 'Error: Matrices must have the same dimensions.'
-        ENDIF
-
-       ELSE IF (iopt .EQ. 3) THEN
-        CALL read_matrix(A, m1, n1, 'A')
-        CALL read_matrix(B, m2, n2, 'B')
-        IF (n1 .EQ. m2) THEN
-         CALL multiply_matrix(A, B, C, m1, n1, n2)
-         CALL print_matrix(A, m1, n1, 'A')
-         CALL print_matrix(B, m2, n2, 'B')
-         CALL print_matrix(C, m1, n2, 'A * B')
-        ELSE
-         PRINT *, 'Error: Inner dimensions must match.'
-        ENDIF
-
-       ELSE IF (iopt .EQ. 4) THEN
-        CALL read_matrix(A, m1, n1, 'A')
-        CALL transpose_matrix(A, C, m1, n1)
-        CALL print_matrix(A, m1, n1, 'A')
-        CALL print_matrix(C, n1, m1, 'Transpose(A)')
-
-       ELSE IF (iopt .EQ. 5) THEN
-        done = .TRUE.
-
-       ELSE
-        PRINT *, 'Invalid option. Try again.'
-       ENDIF
+         CALL print_matrix(C, n1, m1, 'Transpose(A)')
+        CASE (5)
+         PRINT *, 'Exiting the program...'
+         PRINT *
+         quit = .TRUE.
+        CASE DEFAULT
+         PRINT *, 'Invalid choice. Please try again.'
+       END SELECT
       END DO
       END PROGRAM
 
-      SUBROUTINE show_menu(iopt)
-       INTEGER iopt
+c Prints a pretty operation menu, reads user choice
+      SUBROUTINE menu(choice)
+       INTEGER choice
        PRINT *
        PRINT *, '--------------------------------------'
-       PRINT *, ' MATRIX OPERATIONS MENU'
+       PRINT *, 'MATRIX OPERATIONS'
        PRINT *, '--------------------------------------'
        PRINT *, '1. Matrix Addition'
        PRINT *, '2. Matrix Subtraction'
@@ -76,18 +78,23 @@ c ----------------------------------------------
        PRINT *, '4. Matrix Transpose'
        PRINT *, '5. Quit'
        PRINT *, 'Enter your choice (1-5):'
-       READ *, iopt
+       READ *, choice
        RETURN
       END SUBROUTINE
 
+c Reads data into one matrix
+c Note: we added an allocatable string for the matrix name
+c (This made the outputs significantly more clear)
       SUBROUTINE read_matrix(X, m, n, name)
        REAL, DIMENSION(10, 10) :: X
        CHARACTER*(*) name
        INTEGER i, j, m, n
        PRINT *
-       PRINT *, 'Enter number of rows and columns for matrix ', name,':'
-       READ *, m, n
-       PRINT *, 'Enter elements of matrix ', name, ' (row by row):'
+       PRINT *, 'Enter number of rows for matrix ', name, ':'
+       READ *, m
+       PRINT *, 'Enter number of columns for matrix ', name, ':'
+       READ *, n
+       PRINT *, 'Enter elements of matrix ', name, ' (one at a time):'
        DO i = 1, m
         DO j = 1, n
          READ *, X(i, j)
@@ -96,21 +103,26 @@ c ----------------------------------------------
        RETURN
       END SUBROUTINE
 
+c Prints one matrix row by row
+c Note: we added an allocatable string for the matrix name
+c (This made the outputs significantly more clear)
       SUBROUTINE print_matrix(X, m, n, name)
        REAL, DIMENSION(10, 10) :: X
        CHARACTER*(*) name
-       INTEGER i, j
+       INTEGER i, j, m, n
        PRINT *
-       PRINT *, 'Matrix ', name, ':'
+  100  FORMAT(A, A, ' (', I2, ' x ', I2, '):')
+       PRINT 100, 'Matrix ', name, m, n
        DO i = 1, m
         WRITE (*, '(10F10.3)') (X(i, j), j = 1, n)
        END DO
        RETURN
       END SUBROUTINE
 
-      SUBROUTINE add_matrix(A, B, C, m, n)
+c Adds two matrices, stores result in a third matrix
+      SUBROUTINE add_matrices(A, B, C, m, n)
        REAL, DIMENSION(10, 10) :: A, B, C
-       INTEGER i, j
+       INTEGER i, j, m, n
        DO i = 1, m
         DO j = 1, n
          C(i, j) = A(i, j) + B(i, j)
@@ -119,9 +131,10 @@ c ----------------------------------------------
        RETURN
       END SUBROUTINE
 
-      SUBROUTINE subtract_matrix(A, B, C, m, n)
+c Subtracts two matrices, stores result in a third matrix
+      SUBROUTINE subtract_matrices(A, B, C, m, n)
        REAL, DIMENSION(10, 10) :: A, B, C
-       INTEGER i, j
+       INTEGER i, j, m, n
        DO i = 1, m
         DO j = 1, n
          C(i, j) = A(i, j) - B(i, j)
@@ -130,13 +143,14 @@ c ----------------------------------------------
        RETURN
       END SUBROUTINE
 
-      SUBROUTINE multiply_matrix(A, B, C, m, n, p)
+c Multiplies two matrices (dot product), stores result in a third matrix
+      SUBROUTINE multiply_matrices(A, B, C, m, n1, n2)
        REAL, DIMENSION(10, 10) :: A, B, C
-       INTEGER i, j, k, m, n, p
+       INTEGER i, j, k, m, n1, n2
        DO i = 1, m
-        DO j = 1, p
+        DO j = 1, n2
          C(i, j) = 0.0
-         DO k = 1, n
+         DO k = 1, n1
           C(i, j) = C(i, j) + A(i, k) * B(k, j)
          END DO
         END DO
@@ -144,12 +158,13 @@ c ----------------------------------------------
        RETURN
       END SUBROUTINE
 
-      SUBROUTINE transpose_matrix(A, C, m, n)
-       REAL, DIMENSION(10, 10) :: A, C
-       INTEGER i, j
+c Transposes one matrix, stores result in a second matrix
+      SUBROUTINE transpose_matrix(A, B, m, n)
+       REAL, DIMENSION(10, 10) :: A, B
+       INTEGER i, j, m, n
        DO i = 1, m
         DO j = 1, n
-         C(j, i) = A(i, j)
+         B(j, i) = A(i, j)
         END DO
        END DO
        RETURN
