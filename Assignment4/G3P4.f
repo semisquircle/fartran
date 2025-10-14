c0f56d4251d45a05b447628b03bcc6211336900d
semisquircle <semisquircle@gmail.com>
2025-10-13 21:28:42 -0400

    Create G3P4.f

---
+++ Assignment4/G3P4.f
@@ -0,0 +1,8 @@
+      PROGRAM G3P3
+c ----------------------------------------------
+c Assignment 4: The Matrix Calculator Program
+c Group 3 (Shawn Gallagher, Lucas Giovannelli)
+c ----------------------------------------------
+      IMPLICIT NONE
+
+      END
