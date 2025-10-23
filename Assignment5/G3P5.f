8912ab8d565d426160f5ade28c94bbe50be8dfb6
AhabNStarbuck <GIO07221@pennwest.edu>
2025-10-22 22:46:57 -0400

    Update G3P5.f

--- Assignment5/G3P5.f
+++ Assignment5/G3P5.f
@@ -4,5 +4,31 @@ c Assignment 5: The Graphing Program
 c Group 3 (Shawn Gallagher, Lucas Giovannelli)
 c ----------------------------------------------
       IMPLICIT NONE
+c prompt for file
+LOGICAL quit
+
+DO WHILE (.NOT. quit .AND. .NOT. file_in_exists)
+       PRINT *, 'Please enter an input file name (or QUIT to exit):'
+       READ(*, *) file_name_in
+       IF (file_name_in .EQ. 'QUIT') THEN
+        quit = .TRUE.
+       ELSE
+        INQUIRE(FILE=file_name_in, EXIST=file_in_exists)
+        IF (file_in_exists) THEN
+         PRINT *, 'Input file found! Reading data...'
+         OPEN(UNIT=10, FILE=file_name_in, STATUS='OLD', IOSTAT=flag)
+         PRINT *
+        ELSE
+         PRINT *, 'File does not exist...'
+        END IF
+       END IF
+      END DO
+
+INTERGER, XY = 0
+PRINT 'Enter the number of xy points in the file. (can't be more than 100)'
+DO 
+READ XY
+IF (XY .GT. 100)
+PRINT 
 
       END PROGRAM
