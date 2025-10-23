79956b23006caa6cd5b2ce224f369cfed6af8709
semisquircle <semisquircle@gmail.com>
2025-10-23 01:54:39 -0400

    shawn fix

--- Assignment5/G3P5_shawn.f
+++ Assignment5/G3P5_shawn.f
@@ -7,9 +7,9 @@ c ----------------------------------------------
 
       INTEGER, PARAMETER :: max_points = 100
       REAL :: x(max_points), y(max_points)
-      REAL xmin, xmax, ymin, ymax, xstep, ystep, dx
+      REAL xmin, xmax, ymin, ymax, dx, dy, tdx
       CHARACTER*100 file_name_in, file_name_out, out_choice
-      INTEGER n, i, lines_to_plot, flag
+      INTEGER n, i, num_lines, flag
       LOGICAL file_in_exists, file_out_exists, overwrite, quit
 
       file_in_exists = .FALSE.
@@ -52,7 +52,7 @@ c Prompt for output file (handle all cases)
          SELECT CASE (out_choice)
          CASE ('OVERWRITE')
           PRINT *, 'Overwriting output data...'
-          OPEN(UNIT=20, FILE=out_choice, STATUS='OLD', IOSTAT=flag)
+          OPEN(UNIT=20, FILE=file_name_out, STATUS='OLD', IOSTAT=flag)
          CASE ('QUIT')
           quit = .TRUE.
          CASE DEFAULT
@@ -69,26 +69,53 @@ c Prompt for output file (handle all cases)
       END IF
 
 c Read number of points + the points themselves
-      READ(10, *, IOSTAT=flag) n
-      DO i = 1, n
-       READ(10, *, IOSTAT=flag) x(i), y(i)
-      END DO
+      IF (.NOT. quit) THEN
+       READ(10, *, IOSTAT=flag) n
+       DO i = 1, n
+        READ(10, *, IOSTAT=flag) x(i), y(i)
+       END DO
 
 c Write the unsorted and unsorted points
-      WRITE(20, *) 'DATA POINTS'
-      DO i = 1, n
-       WRITE(20, '(2F15.8)') x(i), y(i)
-      END DO
+       WRITE(20, *) 'DATA POINTS'
+       DO i = 1, n
+        WRITE(20, '(2F15.8)') x(i), y(i)
+       END DO
 
-      CALL sort_xy(x, y, n)
-      WRITE(20, *) 'SORTED DATA'
-      DO i = 1, n
-       WRITE(20, '(2F15.8)') x(i), y(i)
-      END DO
+       CALL sort_xy(x, y, n)
+       WRITE(20, *) 'SORTED DATA'
+       DO i = 1, n
+        WRITE(20, '(2F15.8)') x(i), y(i)
+       END DO
 
-c ACTUAL PLOT STUFF WOULD GO HERE...
+       xmin = MINVAL(X(1:n))
+       xmax = MAXVAL(X(1:n))
+       ymin = MINVAL(Y(1:n))
+       ymax = MAXVAL(Y(1:n))
+       dy = (ymax - ymin) / 10.0
+       CALL write_y_scale(ymin, ymax, dy)
+
+       dx = x(2) - x(1)
+       DO i = 2, n - 1
+        tdx = x(i + 1) - x(i)
+        IF (tdx .NE. 0 .AND. tdx .LT. dx) THEN
+         dx = tdx
+        END IF
+       END DO
+
+       num_lines = (xmax - xmin) / dx + 1
+       IF (num_lines .LT. 20) THEN
+        num_lines = 20
+        dx = (xmax - xmin) / 21
+       ELSE IF (num_lines .GT. 200) THEN
+        num_lines = 20
+        dx = (xmax - xmin) / 201
+       END IF
+
+       CALL plot(x, y, n, xmin, xmax, ymin, ymax, dx, dy)
+      END IF
 
       END PROGRAM
+
 
       SUBROUTINE sort_xy(x, y, n)
        INTEGER n, i, j
@@ -98,32 +125,34 @@ c ACTUAL PLOT STUFF WOULD GO HERE...
        DO i = 1, n - 1
         DO j = i + 1, n
          IF (x(j) .LT. x(i)) THEN
-          temp_x = x(i); temp_y = y(i)
-          x(i) = x(j); y(i) = y(j)
-          x(j) = temp_x; y(j) = temp_y
+          temp_x = x(i)
+          temp_y = y(i)
+          x(i) = x(j)
+          y(i) = y(j)
+          x(j) = temp_x
+          y(j) = temp_y
          END IF
         END DO
        END DO
       END SUBROUTINE
 
-      SUBROUTINE write_y_scale(ymin, ymax, ystep)
-       REAL ymin, ymax, ystep
-       WRITE(20, '(6E10.4)') (ymin + (i - 1) * ystep, i = 1, 6)
+      SUBROUTINE write_y_scale(ymin, ymax, dy)
+       REAL ymin, ymax, dy
+       WRITE(20, '(6E10.4)') (ymin + (i - 1) * dy, i = 1, 6)
       END SUBROUTINE
 
-      SUBROUTINE plot(x, y, n, xmin, xmax, ymin, ymax, dx, ystep)
-       INTEGER n
-       REAL x(n), y(n), xmin, xmax, ymin, ymax, dx, ystep, xp, yp, ytick
-       INTEGER i, j
+      SUBROUTINE plot(x, y, n, xmin, xmax, ymin, ymax, dx, dy)
+      INTEGER n, i, j
+       REAL x(n), y(n), xmin, xmax, ymin, ymax, dx, dy, xp, yp, ytick
        LOGICAL printed
 
-       DO yp = ymin, ymax, ystep
+       DO yp = ymin, ymax, dy
         WRITE(20, '(F10.4,"+")', ADVANCE='no') yp
-        DO xp = xmin, xmax, (xmax-xmin) / 60.0
+        DO xp = xmin, xmax, (xmax - xmin) / 60.0
          printed = .FALSE.
          DO j = 1, n
           IF (abs(x(j) - xp) .LT. dx / 2 .AND. abs(y(j) - yp) .LT.
-     &    ystep / 2) THEN
+     &    dy / 2) THEN
            WRITE(20, '(A)', ADVANCE='no') '*'
            printed = .TRUE.
            EXIT
