dc8ffae04b17638a6720890471ef8719a2d3b07c
semisquircle <semisquircle@gmail.com>
2025-11-04 06:50:48 -0500

    Create G3P6.f

---
+++ Assignment6/G3P6.f
@@ -0,0 +1,8 @@
+      PROGRAM G3P5
+c ----------------------------------------------
+c Assignment 6: The Complex Equation Solver
+c Group 3 (Shawn Gallagher, Lucas Giovannelli)
+c ----------------------------------------------
+      IMPLICIT NONE
+
+      END PROGRAM
